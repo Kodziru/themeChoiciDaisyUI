4503b30068cd092bf62688e9fd84bd12c4c3718c
kodziru <kodzirun@gmail.com>
2024-11-06 23:16:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from "react";
+
+function App() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-base-100 text-base-content">
+            <h1 className="text-4xl font-bold text-primary">
+                Bienvenue sur mon site !
+            </h1>
+            <button className="btn btn-primary mt-4">Bouton DaisyUI</button>
+        </div>
+    );
+}
+
+export default App;
